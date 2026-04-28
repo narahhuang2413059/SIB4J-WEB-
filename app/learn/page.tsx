1b54d66515ba87c76562108d9fe290fb1a9eceb6
narahhuang2413059 <2413059@students.universitasmulia.ac.id>
2026-04-29 01:12:30 +0800

    my-app

---
+++ app/learn/page.tsx
@@ -0,0 +1,14 @@
+export default function LearnPage() {
+    return (
+        <div>
+        <div>
+            <img></img>
+            <p>Design</p>
+            </div>
+            <div>
+            <h1>Figma</h1>
+            <p>8 Hours ago</p>
+            </div>
+        </div>
+    );
+}
