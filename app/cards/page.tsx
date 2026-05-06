078f7584c227e2c1ad02120755cdda49e46efc7b
NARAH HUANG <2413059@students.universitasmulia.ac.id>
2026-05-06 22:20:27 +0800

    Add files via upload

---
+++ app/cards/page.tsx
@@ -0,0 +1,14 @@
+export default function Cards() {
+  return (
+    <div className="bg-gray-200 h-screen flex items-center justify-center text-blue-950 flex-wrap gap-5">
+        <div ClassName="bg-white-100 p-30 rounded-2x1">
+             <div ClassName="bg-white p-2 rounded-2x1">
+            <h2 ClassName="font-bold">Web design</h2>
+            <p>Crafts engaging, user-friendly websites.</p>
+
+              </div>
+              
+           </div>
+    </div>
+  );
+}
